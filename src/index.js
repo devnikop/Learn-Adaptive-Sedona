bbdb10fa0bba01113043127d985822d6580a7c80
devnikop <petrov.petrov.ak@gmail.com>
2019-11-11 10:36:33 +0300

    add styles for photo.html

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 "use strict";
 
 const jsEnable = document.querySelector(".no-script");
-jsEnable.classList.remove("no-script");
+if (jsEnable) {
+  jsEnable.classList.remove("no-script");
+}
 
 const navigationElement = document.querySelector(".navigation");
-const navigationOpenElement = navigationElement.querySelector(
-  ".navigation__open"
-);
-const navigationCloseElement = navigationElement.querySelector(
-  ".navigation__close"
-);
+if (navigationElement) {
+  const navigationOpenElement = navigationElement.querySelector(
+    ".navigation__open"
+  );
+  const navigationCloseElement = navigationElement.querySelector(
+    ".navigation__close"
+  );
 
-navigationElement.classList.add("navigation--closed");
-
-navigationOpenElement.addEventListener("click", () => {
-  navigationElement.classList.remove("navigation--closed");
-});
-
-navigationCloseElement.addEventListener("click", () => {
   navigationElement.classList.add("navigation--closed");
-});
+
+  if (navigationOpenElement && navigationCloseElement) {
+    navigationOpenElement.addEventListener("click", () => {
+      navigationElement.classList.remove("navigation--closed");
+    });
+
+    navigationCloseElement.addEventListener("click", () => {
+      navigationElement.classList.add("navigation--closed");
+    });
+  }
+}
